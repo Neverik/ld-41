6aae3007fbfb431dbc96b332dc77bdadf62b803b
neverik <neverliberty@gmail.com>
2018-05-01 20:59:00 +0700

    My heavily unfinished and buggy LD 41 game's source code.

---
+++ SlideShow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SlideShow : MonoBehaviour {
+
+    public Image img;
+    public Sprite[] sprites;
+    public string nextScene;
+    int index;
+    public bool loadNext = true;
+
+    void Update() {
+        img.sprite = sprites[index];
+    }
+
+    public void NextSlide() {
+        index++;
+        if (index == sprites.Length) {
+            index--;
+            if (loadNext)
+            {
+                SceneManager.LoadScene(nextScene);
+            }
+        }
+    }
+}
